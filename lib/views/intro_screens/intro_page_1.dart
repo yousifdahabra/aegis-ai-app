de3b5cf83c7a9665536b08336fec0db03ada5ec6
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-25 21:34:03 +0200

    App - update intro page 1 set Scaffold

--- lib/views/intro_screens/intro_page_1.dart
+++ lib/views/intro_screens/intro_page_1.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
 class IntroPage1 extends StatelessWidget {
@@ -5,11 +6,6 @@ class IntroPage1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      child: Center(
-        child: Text("Page 1"),
-      ),
-    );
+    return Scaffold();
   }
 }
