6dd8ccb7238be4e554b25b08c02cdc116cfe1f86
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-25 21:37:18 +0200

    App - update intro page 1 set sub title

--- lib/views/intro_screens/intro_page_1.dart
+++ lib/views/intro_screens/intro_page_1.dart
@@ -39,6 +39,22 @@ class IntroPage1 extends StatelessWidget {
                       ),
                     )),
               ),
+              Positioned(
+                top: 120,
+                left: 10,
+                child: Container(
+                  margin: EdgeInsets.only(top: 100),
+                  child: Center(
+                    child: Text(
+                      "Everyone has a weakness, whether it's\nme or others.",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ]),
           ),
         ],
