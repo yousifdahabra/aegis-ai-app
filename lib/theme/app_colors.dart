e47c6dae51c2c53fdd9cddb1c67dea16370441e8
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-29 15:57:31 +0200

    App - update app_colors add mainTitleColor

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const backgroundPageColor = Color(0xFFFFFFFF);
   static const linksColor = Color(0xFF289DD2);
+  static const mainTitleColor = Color(0xFF16354D);
 }
