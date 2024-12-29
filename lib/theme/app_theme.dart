d183879d4a072dd71b69a6ee8d3eab6f4cb1e523
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-29 17:27:41 +0200

    App - update AppTheme add textTheme add textTheme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'app_colors.dart';
+import 'app_fonts.dart';
 
 class AppTheme {
   static ThemeData get lightTheme {
     return ThemeData(
       primaryColor: AppColors.mainTitleColor,
       scaffoldBackgroundColor: AppColors.backgroundPageColor,
+      textTheme: TextTheme(
+        displayLarge: TextStyle(
+          fontFamily: AppFonts.mainTitleFont,
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: AppColors.mainTitleColor,
+        ),
+      ),
     );
   }
 }
