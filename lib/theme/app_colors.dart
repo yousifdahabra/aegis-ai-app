52a5c77b1eaa1fb4066dfc7dbb8e1b9e21b9a171
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-29 15:58:03 +0200

    App - update app_colors add fillTextInputColor

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -6,4 +6,5 @@ class AppColors {
   static const mainTitleColor = Color(0xFF16354D);
   static const fillInputsColor = Color(0xFFF7F8FA);
   static const fillButtonColor = Color(0xFFFFFFFF);
+  static const fillTextInputColor = Color(0xFF8F9098);
 }
