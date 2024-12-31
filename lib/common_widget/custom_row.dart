61ebf972441e642939b45a45427784d79eb094d6
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-31 19:34:54 +0200

    App - update CustomRow add Row

--- lib/common_widget/custom_row.dart
+++ lib/common_widget/custom_row.dart
@@ -5,6 +5,6 @@ class CustomRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: []);
   }
 }
