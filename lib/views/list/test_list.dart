966e1d3318a24283ef5bc3815ce083050f4e2b12
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-31 20:00:37 +0200

    App - update TestList add data array

--- lib/views/list/test_list.dart
+++ lib/views/list/test_list.dart
@@ -5,13 +5,27 @@ class TestList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, String>> data = [
+      {
+        'title': 'Test Title',
+        'security': '75%',
+        'status': 'Done',
+        'questions': '10',
+      },
+      {
+        'title': 'Another Test',
+        'security': '80%',
+        'status': 'Completed',
+        'questions': '12',
+      },
+    ];
     return Scaffold(
       appBar: AppBar(
         title: const Text("Test List"),
       ),
       body: ListView.builder(
         padding: const EdgeInsets.all(8.0),
-        itemCount: 12,
+        itemCount: data.length,
         itemBuilder: (context, index) {},
       ),
     );
