098ddddf4d729cec649c3ed36137be1b75efa0d3
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-31 02:03:54 +0200

    App - update custom_button add    _buildButtonStyle in ElevatedButton

--- lib/common_widget/custom_button.dart
+++ lib/common_widget/custom_button.dart
@@ -26,6 +26,14 @@ class CustomButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color(0xFF16354D),
+        padding:
+            padding ?? const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
       onPressed: onPressed,
       child: Text(
         text,
