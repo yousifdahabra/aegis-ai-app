af2db91ea9d138d721eced35c532913a9d372325
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-31 20:00:05 +0200

    App - update TestList add ListView.builder

--- lib/views/list/test_list.dart
+++ lib/views/list/test_list.dart
@@ -5,6 +5,15 @@ class TestList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Test List"),
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8.0),
+        itemCount: 12,
+        itemBuilder: (context, index) {},
+      ),
+    );
   }
 }
