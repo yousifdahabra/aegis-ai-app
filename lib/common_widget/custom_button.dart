cec3f35522f398cba794604e4966a038f687f10c
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-31 02:02:56 +0200

    App - update custom_button add title text,onPressed and padding

--- lib/common_widget/custom_button.dart
+++ lib/common_widget/custom_button.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+    this.padding,
+  }) : super(key: key);
+
+  final String text;
+  final VoidCallback? onPressed;
+  final EdgeInsetsGeometry? padding;
+
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: onPressed,
       child: Text(
-        'Title',
+        text,
         style: Theme.of(context).textTheme.displaySmall,
       ),
     );
