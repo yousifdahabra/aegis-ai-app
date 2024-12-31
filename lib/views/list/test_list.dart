b8982666ca685b08081932500661f40af5946b70
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-31 19:59:12 +0200

    App - add TestList

---
+++ lib/views/list/test_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TestList extends StatelessWidget {
+  const TestList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
