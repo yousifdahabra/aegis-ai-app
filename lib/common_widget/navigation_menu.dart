acd443c843bd3c8a782a064746158ca4390cf246
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-01 16:21:38 +0200

    App - update Navigation Menu update from StatelessWidget  to StatefulWidget

--- lib/common_widget/navigation_menu.dart
+++ lib/common_widget/navigation_menu.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class NavigationMenu extends StatelessWidget {
+class NavigationMenu extends StatefulWidget {
   const NavigationMenu({super.key});
 
+  @override
+  State<NavigationMenu> createState() => _NavigationMenuState();
+}
+
+class _NavigationMenuState extends State<NavigationMenu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
