d08bc6243767b380458e8f349113d115cd52c467
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-01 20:37:35 +0200

    App - add AddExpertRequest

---
+++ lib/views/list/add_expert_request.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddExpertRequest extends StatelessWidget {
+  const AddExpertRequest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
