ade70c5ae04a4db233fbab03163701bb1aca5f77
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-01 15:33:49 +0200

    App - update NavigationMenu add bottomNavigationBar

---
+++ lib/common_widget/navigation_menu.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          Container(),
+        ],
+      ),
+    );
+  }
+}
