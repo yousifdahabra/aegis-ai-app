6b1f298ac501d0d59b412e9377b90e903fa28fd3
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-01 20:39:40 +0200

    App - update AddExpertRequest add Scaffold with AppBar

--- lib/views/list/add_expert_request.dart
+++ lib/views/list/add_expert_request.dart
@@ -5,6 +5,19 @@ class AddExpertRequest extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text("New Request", style: TextStyle(fontSize: 22)),
+            SizedBox(height: 4),
+            Text("Better test will be with more information",
+                style: TextStyle(fontSize: 14)),
+          ],
+        ),
+      ),
+    );
   }
 }
