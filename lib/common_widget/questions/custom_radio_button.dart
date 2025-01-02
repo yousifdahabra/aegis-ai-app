b9e5c88f1d31f4b151c08517ae020ce2271c16a6
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-02 13:58:53 +0200

    App - add CustomRadioButton

---
+++ lib/common_widget/questions/custom_radio_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomRadioButton extends StatefulWidget {
+  const CustomRadioButton({super.key});
+
+  @override
+  State<CustomRadioButton> createState() => _CustomRadioButtonState();
+}
+
+class _CustomRadioButtonState extends State<CustomRadioButton> {
+  @override
+  Widget build(BuildContext context) {
+    return RadioMenuButton();
+  }
+}
