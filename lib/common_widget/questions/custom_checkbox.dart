8ea4c01ea437bc3d7177bde66c640b90764eded4
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-02 17:31:20 +0200

    App - add CustomCheckbox

---
+++ lib/common_widget/questions/custom_checkbox.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomCheckbox extends StatefulWidget {
+  const CustomCheckbox({super.key});
+
+  @override
+  State<CustomCheckbox> createState() => _CustomCheckboxState();
+}
+
+class _CustomCheckboxState extends State<CustomCheckbox> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
