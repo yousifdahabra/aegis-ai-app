a38c9d490e4079284ba0d3487b0efb422f9bffae
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-02 15:50:28 +0200

    App - update CustomRadioButton update RadioMenuButton - wrap with Scaffold and Padding

--- lib/common_widget/questions/custom_radio_button.dart
+++ lib/common_widget/questions/custom_radio_button.dart
@@ -11,31 +11,48 @@ class _CustomRadioButtonState extends State<CustomRadioButton> {
   String selectedOption = 'None Select';
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          width: 300,
-          child: RadioMenuButton(
-            value: "question1",
-            groupValue: selectedOption,
-            onChanged: (selectOption) {},
-            style: ButtonStyle(
-                shape: WidgetStatePropertyAll(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                      10,
-                    ),
-                  ),
-                ),
-                elevation: const WidgetStatePropertyAll(2),
-                backgroundColor: const WidgetStatePropertyAll(Colors.amber)),
-            child: Text(
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
               "question1",
               style: Theme.of(context).textTheme.headlineSmall,
             ),
-          ),
+            SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: 300,
+              child: RadioMenuButton(
+                value: "question1",
+                groupValue: selectedOption,
+                onChanged: (selectValue) {
+                  setState(() => selectedOption = selectValue!);
+                },
+                style: ButtonStyle(
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          10,
+                        ),
+                      ),
+                    ),
+                    elevation: const WidgetStatePropertyAll(2),
+                    backgroundColor:
+                        const WidgetStatePropertyAll(Colors.amber)),
+                child: Text(
+                  "question1",
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
