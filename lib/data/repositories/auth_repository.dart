db19d83c89940566149ec6694e1f948d0d55d247
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-03 12:43:54 +0200

    App - update AuthRepository add signup method

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -1,8 +1,23 @@
 import 'package:dio/dio.dart';
 import '../api/dio_config.dart';
+import '../models/user_model.dart';
+import '../api/api_endpoints.dart';
 
 class AuthRepository {
   final DioConfig _dioClient;
 
   AuthRepository() : _dioClient = DioConfig();
+
+  Future<void> signup(UserModel user) async {
+    try {
+      final response = await _dioClient.dio.post(
+        ApiEndpoints.signup,
+        data: user.toJson(),
+      );
+
+      print('Signup Successful: ${response.data}');
+    } on DioException catch (e) {
+      throw Exception('Signup Failed: ${e.response?.data}');
+    }
+  }
 }
