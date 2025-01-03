cc1167ecfe397ea11a669c775b4808bd5c1b7bda
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-03 12:39:58 +0200

    App - update AuthRepository add dio and dio_config

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -1,1 +1,8 @@
-class AuthRepository {}
+import 'package:dio/dio.dart';
+import '../api/dio_config.dart';
+
+class AuthRepository {
+  final DioConfig _dioClient;
+
+  AuthRepository() : _dioClient = DioConfig();
+}
