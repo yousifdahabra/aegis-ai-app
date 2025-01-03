19f0cbe5e5e6e5b0c88ff4a57ca92c1d1cc32580
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-03 16:32:59 +0200

    App - update SignupEvent add SignupEvent

--- lib/bloc/auth/auth_event.dart
+++ lib/bloc/auth/auth_event.dart
@@ -1,4 +1,16 @@
 part of 'auth_bloc.dart';
 
 @immutable
-sealed class AuthEvent {}
+sealed class AuthEvent {
+  @override
+  List<Object> get props => [];
+}
+
+class SignupEvent extends AuthEvent {
+  final UserModel user;
+
+  SignupEvent(this.user);
+
+  @override
+  List<Object> get props => [user];
+}
