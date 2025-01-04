f1ccd356f87feca46cd01d3f6084e8d97882a91e
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-05 00:45:24 +0200

    App - update CustomAlertDialog add  Text and content

--- lib/common_widget/custom_alert_dialog.dart
+++ lib/common_widget/custom_alert_dialog.dart
@@ -3,6 +3,20 @@ import 'package:flutter/material.dart';
 class CustomAlertDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AlertDialog();
+    return AlertDialog(
+      title: Text(
+        'title',
+        style: TextStyle(
+          color: Colors.red,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      content: Text(
+        'message',
+        style: const TextStyle(
+          fontSize: 16,
+        ),
+      ),
+    );
   }
 }
