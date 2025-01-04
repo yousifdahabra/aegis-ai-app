a76ff3df1b7ed8c79b9840a536de24b175a2bc9d
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-05 00:19:06 +0200

    App - update AuthBloc

--- lib/bloc/auth/auth_bloc.dart
+++ lib/bloc/auth/auth_bloc.dart
@@ -1,37 +1,33 @@
-import 'package:ai_safety_app/data/api/api_endpoints.dart';
 import 'package:bloc/bloc.dart';
-import 'package:dio/dio.dart';
 import 'package:meta/meta.dart';
 import 'package:equatable/equatable.dart';
-import '../../utils/error_handler.dart';
+import '../../data/repositories/auth_repository.dart';
+import '../../data/models/user_model.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  final Dio dio;
+  final AuthRepository authRepository;
 
-  AuthBloc(this.dio) : super(AuthInitial()) {
+  AuthBloc(this.authRepository) : super(AuthInitial()) {
     on<SignupEvent>((event, emit) async {
       emit(AuthLoading());
-      try {
-        final response = await dio.post(
-          ApiEndpoints.signup,
-          data: {
-            'name': event.name,
-            'email': event.email,
-            'password': event.password,
-            'phone_number': '234324234234',
-          },
-        );
-        emit(AuthSuccess(response.data['message']));
-      } catch (error) {
-        if (error is DioException) {
-          final errorMessage = handleDioError(error);
-          emit(AuthFailure(errorMessage));
-        } else {
-          emit(AuthFailure('An unexpected error occurred.'));
-        }
+      final message = await authRepository.signup(
+        UserModel(
+          name: event.name,
+          email: event.email,
+          password: event.password,
+          phoneNumber: '234234',
+          links: '',
+          extraInformations: '',
+        ),
+      );
+
+      if (message['success']) {
+        emit(AuthSuccess(message['message']));
+      } else {
+        emit(AuthFailure(message['message']));
       }
     });
   }
