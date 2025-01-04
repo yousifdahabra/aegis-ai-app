65eeb06647b515ded89778cf268915e7b11a0d16
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-05 00:46:39 +0200

    App - update CustomAlertDialog add parameters

--- lib/common_widget/custom_alert_dialog.dart
+++ lib/common_widget/custom_alert_dialog.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
 
 class CustomAlertDialog extends StatelessWidget {
+  final String title;
+  final String message;
+  final bool isSuccess;
+  final VoidCallback onPressed;
+
+  const CustomAlertDialog({
+    Key? key,
+    required this.title,
+    required this.message,
+    required this.isSuccess,
+    required this.onPressed,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text(
-        'title',
+        title,
         style: TextStyle(
-          color: Colors.red,
+          color: isSuccess ? Colors.green : Colors.red,
           fontWeight: FontWeight.bold,
         ),
       ),
       content: Text(
-        'message',
+        message,
         style: const TextStyle(
           fontSize: 16,
         ),
