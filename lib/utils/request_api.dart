569d8d4d9b5d1efd5081ce0c19c17d333e93ff91
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-04 21:15:52 +0200

    App - update RequestAPI add _handleDioError delete lib\utils\error_handler.dart

--- lib/utils/request_api.dart
+++ lib/utils/request_api.dart
@@ -40,4 +40,25 @@ class RequestAPI {
       };
     }
   }
+
+  String _handleDioError(DioException error) {
+    if (error.response != null) {
+      final responseData = error.response?.data;
+
+      final message = responseData['message'] ?? 'Unknown error occurred';
+
+      if (responseData['errors'] != null) {
+        final errors = responseData['errors'] as Map<String, dynamic>;
+        final errorMessages = errors.entries
+            .map((entry) => '${entry.key}: ${entry.value.join(", ")}')
+            .join("\n");
+
+        return '$message\n$errorMessages';
+      } else {
+        return message;
+      }
+    } else {
+      return error.message ?? 'An unknown network error occurred.';
+    }
+  }
 }
