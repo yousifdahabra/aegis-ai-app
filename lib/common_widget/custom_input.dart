0a934b0111b026943546d1fcd9d3eda9ae593d0e
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-04 11:52:12 +0200

    App - update CustomInput add controller and validator

--- lib/common_widget/custom_input.dart
+++ lib/common_widget/custom_input.dart
@@ -9,6 +9,8 @@ class CustomInput extends StatelessWidget {
     this.type = TextInputType.text,
     this.scure = false,
     this.multiLine = false,
+    this.validator,
+    this.controller,
   }) : super(key: key);
 
   final String label;
@@ -17,37 +19,32 @@ class CustomInput extends StatelessWidget {
   final TextInputType type;
   final bool scure;
   final bool multiLine;
+  final String? Function(String?)? validator;
+  final TextEditingController? controller;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      validator: (value) {
-        if (value == null || value.isEmpty) {
-          return 'Please enter some text';
-        }
-        if (!value.contains('@')) {
-          return 'Please enter a valid email';
-        }
-        return null;
-      },
+      controller: controller,
+      validator: validator,
       obscureText: scure,
       keyboardType: multiLine
           ? TextInputType.multiline
           : (scure ? TextInputType.text : type),
-      minLines: multiLine ? 3 : (scure ? 1 : null),
-      maxLines: multiLine ? 5 : (scure ? 1 : null),
+      minLines: multiLine ? 3 : 1,
+      maxLines: multiLine ? 5 : (scure ? 1 : 1),
       decoration: InputDecoration(
         labelText: label,
         labelStyle: Theme.of(context).textTheme.headlineSmall,
         hintText: hint,
         hintStyle: Theme.of(context).textTheme.bodyLarge,
-        prefixIcon: icon, //Icon(Icons.email)
-        border: OutlineInputBorder(),
-        enabledBorder: OutlineInputBorder(
+        prefixIcon: icon,
+        border: const OutlineInputBorder(),
+        enabledBorder: const OutlineInputBorder(
           borderSide: BorderSide(color: Color(0xFF16354D)),
         ),
         focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Color(0xFF289DD2)),
+          borderSide: const BorderSide(color: Color(0xFF289DD2)),
           borderRadius: BorderRadius.circular(10.0),
         ),
       ),
