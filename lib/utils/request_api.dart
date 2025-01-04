fa0045cbbf2b0a4920a35d01cc90c184cfa92c12
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-04 21:10:55 +0200

    App - update RequestAPI add makeRequest method

--- lib/utils/request_api.dart
+++ lib/utils/request_api.dart
@@ -1,1 +1,15 @@
-class RequestAPI {}
+import 'package:dio/dio.dart';
+
+class RequestAPI {
+  final Dio dio;
+
+  RequestAPI({required this.dio});
+
+  Future<Map<String, dynamic>> makeRequest({
+    required String route,
+    String method = 'GET',
+    Map<String, dynamic>? body,
+    String header = 'application/json',
+    String? token,
+  }) async {}
+}
