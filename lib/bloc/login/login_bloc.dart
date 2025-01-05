8b29cfb6744a61401c00708c63f80f00636ce96a
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-05 14:46:01 +0200

    App - update LoginBloc add AuthRepository

--- lib/bloc/login/login_bloc.dart
+++ lib/bloc/login/login_bloc.dart
@@ -8,9 +8,8 @@ part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginInitial()) {
-    on<LoginEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final AuthRepository authRepository;
+  LoginBloc(this.authRepository) : super(LoginInitial()) {
+    on<LoginEvent>((event, emit) async {});
   }
 }
