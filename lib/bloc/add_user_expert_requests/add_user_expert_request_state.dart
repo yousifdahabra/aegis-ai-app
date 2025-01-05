3d27380dd20496df643713c362facceca75e7395
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-05 20:56:25 +0200

    App - update AddUserExpertRequestState add AddUserExpertRequestLoading and AddUserExpertRequestSuccess

--- lib/bloc/add_user_expert_requests/add_user_expert_request_state.dart
+++ lib/bloc/add_user_expert_requests/add_user_expert_request_state.dart
@@ -2,9 +2,20 @@ part of 'add_user_expert_request_bloc.dart';
 
 sealed class AddUserExpertRequestState extends Equatable {
   const AddUserExpertRequestState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class AddUserExpertRequestInitial extends AddUserExpertRequestState {}
+
+class AddUserExpertRequestLoading extends AddUserExpertRequestState {}
+
+class AddUserExpertRequestSuccess extends AddUserExpertRequestState {
+  final String message;
+
+  AddUserExpertRequestSuccess(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
