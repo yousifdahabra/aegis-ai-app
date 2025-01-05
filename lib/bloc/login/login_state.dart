ca90fc104d6a6d7c9e93e14bf0ce822be49dc4e6
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-05 14:50:26 +0200

    App - update LoginState add LoginLoading and LoginSuccess

--- lib/bloc/login/login_state.dart
+++ lib/bloc/login/login_state.dart
@@ -2,9 +2,20 @@ part of 'login_bloc.dart';
 
 sealed class LoginState extends Equatable {
   const LoginState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class LoginInitial extends LoginState {}
+
+class LoginLoading extends LoginState {}
+
+class LoginSuccess extends LoginState {
+  final String message;
+
+  LoginSuccess(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
