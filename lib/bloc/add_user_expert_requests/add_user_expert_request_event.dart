a4844f7931b6565741e285f0b5a573f179c07855
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-05 20:39:14 +0200

    App - add AddUserExpertRequestEvent

---
+++ lib/bloc/add_user_expert_requests/add_user_expert_request_event.dart
@@ -0,0 +1,8 @@
+part of 'add_user_expert_request_bloc.dart';
+
+sealed class AddUserExpertRequestEvent extends Equatable {
+  const AddUserExpertRequestEvent();
+
+  @override
+  List<Object> get props => [];
+}
