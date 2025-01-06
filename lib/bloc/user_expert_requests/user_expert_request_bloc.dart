e569103a0bfd8037673c8c8895eaf2839aab0d0f
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-06 20:24:50 +0200

    App - update UserExpertRequestBloc add getUserData

--- lib/bloc/user_expert_requests/user_expert_request_bloc.dart
+++ lib/bloc/user_expert_requests/user_expert_request_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:ai_safety_app/data/models/add_user_expert_requests_model.dart';
 import 'package:ai_safety_app/data/repositories/user_expert_requests_repository.dart';
+import 'package:ai_safety_app/utils/functions.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:equatable/equatable.dart';
@@ -16,13 +17,17 @@ class UserExpertRequestBloc
     on<AddUserExpertRequestEvent>((event, emit) async {
       emit(AddUserExpertRequestLoading());
 
+      final userData = await Functions.getUserData();
+      final userId = userData?['id'];
+
       final message = await userExpertRequestRepository
           .addUserExpertRequest(AddUserExpertRequestModel(
         about_user: event.aboutUser,
         user_note: event.userNote,
         links: event.links,
-        user_id: 1,
+        user_id: userId,
       ));
+
       if (message['success']) {
         emit(UserExpertRequestSuccess(message['message']));
       } else {
