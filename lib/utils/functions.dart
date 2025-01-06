c16d9f397830e0b803fea72921e05f032208dff9
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-06 20:15:01 +0200

    App - update utils Functions add getUserData

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -1,5 +1,11 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'dart:convert';
 
 class Functions {
   static final FlutterSecureStorage _secureStorage = FlutterSecureStorage();
+
+  static Future<Map<String, dynamic>?> getUserData() async {
+    final userJson = await _secureStorage.read(key: 'user_data');
+    return userJson != null ? jsonDecode(userJson) : null;
+  }
 }
