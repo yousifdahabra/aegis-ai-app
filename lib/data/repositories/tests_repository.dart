2c6365d7ffa7bd4ecf05238b3a5aa88c701356bd
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-07 06:37:03 +0200

    App - update TestsRepository add getTestList

--- lib/data/repositories/tests_repository.dart
+++ lib/data/repositories/tests_repository.dart
@@ -5,4 +5,33 @@ class TestsRepository {
   final DioConfig _dioClient;
 
   TestsRepository() : _dioClient = DioConfig();
+
+  Future<Map<String, dynamic>> getTestList(TestsRepository userRequest) async {
+    try {
+      final response = await _dioClient.api.makeRequest(
+        route: ApiEndpoints.geTesttList,
+        method: 'GET',
+      );
+
+      if (response['success']) {
+        return {
+          'data': null,
+          'success': true,
+          'message': response['message'],
+        };
+      } else {
+        return {
+          'data': null,
+          'success': false,
+          'message': response['message'],
+        };
+      }
+    } catch (e) {
+      return {
+        'data': null,
+        'success': false,
+        'message': 'An unexpected error occurred. $e',
+      };
+    }
+  }
 }
