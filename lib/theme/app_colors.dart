e582fc19c7c18cdcf741ac90ae2b9a23e7d317f8
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-07 20:06:42 +0200

    App - update AppColors add backgroundPageColor

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const backgroundPageColor = Color(0xFFFFFFFF);
+  static const barTitle = Color(0xFFFFFFFF);
   static const linksColor = Color(0xFF289DD2);
   static const mainTitleColor = Color(0xFF16354D);
   static const fillInputsColor = Color(0xFFF7F8FA);
