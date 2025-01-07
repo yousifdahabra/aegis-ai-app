ccee40ae5b8571c288f85c18ee1b9f048d0dd087
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-07 07:43:31 +0200

    App - update TestsBloc update getTestList

--- lib/bloc/tests/tests_bloc.dart
+++ lib/bloc/tests/tests_bloc.dart
@@ -1,5 +1,5 @@
 import 'package:ai_safety_app/data/repositories/tests_repository.dart';
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
@@ -10,13 +10,19 @@ class TestsBloc extends Bloc<TestsEvent, TestsState> {
   final TestsRepository testsRepository;
 
   TestsBloc(this.testsRepository) : super(TestsListInitial()) {
-    on<TestsEvent>((event, emit) async {
+    on<TestsListEvent>((event, emit) async {
       emit(TestsListLoading());
-      final message = await testsRepository.getTestList();
-      if (message['success']) {
-        emit(TestsListSuccess(message['message']));
-      } else {
-        emit(TestsListFailure(message['message']));
+      try {
+        final response = await testsRepository.getTestList();
+        if (response['success'] == true) {
+          final data = response['data'] as List<dynamic>;
+          emit(TestsListSuccess(data: data));
+        } else {
+          emit(TestsListFailure(
+              message: response['message'] ?? 'Unknown error'));
+        }
+      } catch (e) {
+        emit(TestsListFailure(message: e.toString()));
       }
     });
   }
