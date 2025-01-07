3c2a09a723d91bd4af362940d43149ba44e9e9ef
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-07 20:52:27 +0200

    App - update TestPage add body

--- lib/views/test_page.dart
+++ lib/views/test_page.dart
@@ -1,5 +1,7 @@
 import 'package:ai_safety_app/common_widget/custom_app_bar.dart';
+import 'package:ai_safety_app/common_widget/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:ai_safety_app/theme/app_colors.dart';
 
 class TestPage extends StatelessWidget {
   const TestPage({super.key});
@@ -12,6 +14,31 @@ class TestPage extends StatelessWidget {
         subTitle: 'All previous tests are listed here',
         icon: Icons.quiz,
         onIconPressed: () {},
+      ),
+      body: Container(
+        width: double.infinity,
+        color: AppColors.backgroundPageColor,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Welcome to the Aegis AI Test",
+                style: Theme.of(context).textTheme.displayMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Click the button below to start the test. Answer the questions to the best of your ability.",
+                style: Theme.of(context).textTheme.titleSmall,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+            ],
+          ),
+        ),
       ),
     );
   }
