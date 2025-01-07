53c8274f0c11cf68c1269b9dfa923b17579bdef7
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-07 06:40:33 +0200

    App - update TestsState add TestsListSuccess

--- lib/bloc/tests/tests_state.dart
+++ lib/bloc/tests/tests_state.dart
@@ -2,9 +2,20 @@ part of 'tests_bloc.dart';
 
 sealed class TestsState extends Equatable {
   const TestsState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-final class TestsInitial extends TestsState {}
+final class TestsListInitial extends TestsState {}
+
+class TestsListLoading extends TestsState {}
+
+class TestsListSuccess extends TestsState {
+  final String message;
+
+  TestsListSuccess(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
