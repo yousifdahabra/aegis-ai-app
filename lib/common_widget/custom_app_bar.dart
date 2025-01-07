d9321f5547a3f9814b26063e9bf0e2037f67a9e2
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-07 20:38:27 +0200

    App - update CustomAppBar add appbar test list

--- lib/common_widget/custom_app_bar.dart
+++ lib/common_widget/custom_app_bar.dart
@@ -1,10 +1,41 @@
-import 'package:flutter/widgets.dart';
+import 'package:ai_safety_app/theme/app_colors.dart';
+import 'package:flutter/material.dart';
 
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({
+    super.key,
+    required this.mainTitle,
+    this.subTitle,
+  });
+
+  final String mainTitle;
+  final String? subTitle;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AppBar(
+      toolbarHeight: 80,
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            mainTitle,
+            style: Theme.of(context).textTheme.headlineLarge,
+          ),
+          if (subTitle != null) ...[
+            const SizedBox(height: 4),
+            Text(
+              subTitle!,
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+          ],
+        ],
+      ),
+      backgroundColor: AppColors.mainTitleColor,
+      elevation: 0,
+    );
   }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(80);
 }
