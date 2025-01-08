03d1d0dbd65c028a8f94207a94858a8a56101c9a
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-08 19:56:56 +0200

    App - update CustomQuestionInput add questionText

--- lib/common_widget/questions/custom_question_input.dart
+++ lib/common_widget/questions/custom_question_input.dart
@@ -1,53 +1,30 @@
-import 'package:ai_safety_app/common_widget/custom_button.dart';
 import 'package:ai_safety_app/common_widget/custom_input.dart';
-import 'package:ai_safety_app/common_widget/questions/custom_result.dart';
 import 'package:flutter/material.dart';
 
-class CustomQuestionInput extends StatefulWidget {
-  const CustomQuestionInput({super.key});
+class CustomQuestionInput extends StatelessWidget {
+  final Map<String, dynamic> data;
 
-  @override
-  State<CustomQuestionInput> createState() => _CustomQuestionInputState();
-}
-
-class _CustomQuestionInputState extends State<CustomQuestionInput> {
-  final List<String> selectedOptions = [];
+  const CustomQuestionInput({super.key, required this.data});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Question'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            CustomInput(
-              label: 'question here',
-              hint: 'write here',
-              icon: Icon(Icons.question_answer),
-              multiLine: true,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            CustomButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return CustomResult();
-                    },
-                  ),
-                );
-              },
-              text: 'Finish',
-            ),
-          ],
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            data['questionText'] ?? 'Question',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          const SizedBox(height: 20),
+          CustomInput(
+            label: 'Your Answer',
+            hint: 'Type your answer here',
+            icon: const Icon(Icons.question_answer),
+            multiLine: true,
+          ),
+        ],
       ),
     );
   }
