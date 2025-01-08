d2f6cf0c2c38a9c2937a9e67d6a2cacf9ba89b29
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-08 17:24:19 +0200

    App - update StartTestState add StartTestLoading and StartTestSuccess

--- lib/bloc/bloc/start_test_state.dart
+++ lib/bloc/bloc/start_test_state.dart
@@ -2,9 +2,20 @@ part of 'start_test_bloc.dart';
 
 sealed class StartTestState extends Equatable {
   const StartTestState();
-  
+
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
 final class StartTestInitial extends StartTestState {}
+
+class StartTestLoading extends StartTestState {}
+
+class StartTestSuccess extends StartTestState {
+  final Map<String, dynamic> data;
+
+  const StartTestSuccess(this.data);
+
+  @override
+  List<Object?> get props => [data];
+}
