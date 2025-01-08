c3c2fc1883d45fed6a15ca58e18625752ee6250f
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-08 17:58:40 +0200

    App - update TestPage add child with styles and on press

--- lib/views/test_page.dart
+++ lib/views/test_page.dart
@@ -29,6 +29,54 @@ class TestPage extends StatelessWidget {
           );
         }
       },
+      child: Scaffold(
+        appBar: const CustomAppBar(
+          mainTitle: 'Start Test',
+          subTitle: 'Prepare to begin the test',
+        ),
+        body: Container(
+          width: double.infinity,
+          color: AppColors.backgroundPageColor,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Welcome to the Aegis AI Test",
+                  style: Theme.of(context).textTheme.displayMedium,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  "Click the button below to start the test. Answer the questions to the best of your ability.",
+                  style: Theme.of(context).textTheme.titleSmall,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 40),
+                CustomButton(
+                  text: 'Start Test',
+                  onPressed: () {
+                    context.read<StartTestBloc>().add(
+                          StartTest(
+                            title: 'Test Title Example',
+                          ),
+                        );
+                  },
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  "Good luck! We believe in you.",
+                  style: Theme.of(context).textTheme.bodySmall,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
