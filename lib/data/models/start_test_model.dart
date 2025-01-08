34d6a1fd966e7830ff2171868c68645bb8fabc8a
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-08 17:03:36 +0200

    App - update StartTestModel add parameters

--- lib/data/models/start_test_model.dart
+++ lib/data/models/start_test_model.dart
@@ -1,1 +1,23 @@
-class StartTestModel {}
+import 'package:json_annotation/json_annotation.dart';
+
+part 'start_test_model.g.dart';
+
+@JsonSerializable()
+class StartTestModel {
+  final String title;
+  final int user_id;
+  final int expert_id;
+  final int test_state_id;
+
+  StartTestModel({
+    required this.title,
+    required this.user_id,
+    this.expert_id = 3,
+    this.test_state_id = 1,
+  });
+
+  factory StartTestModel.fromJson(Map<String, dynamic> json) =>
+      _$StartTestModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$StartTestModelToJson(this);
+}
