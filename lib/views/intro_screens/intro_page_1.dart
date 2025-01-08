6873078d4734377dd65022f5ee5aa9b0e7cba051
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-08 10:56:56 +0200

    App - update IntroPage1 update styles

--- lib/views/intro_screens/intro_page_1.dart
+++ lib/views/intro_screens/intro_page_1.dart
@@ -1,3 +1,4 @@
+import 'package:ai_safety_app/theme/app_colors.dart';
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
@@ -7,53 +8,9 @@ class IntroPage1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Container(
-            height: 400,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/images/intro/intro-1.png'),
-                  fit: BoxFit.cover),
-            ),
-          ),
-          SizedBox(
-            height: 300,
-            child: Stack(children: <Widget>[
-              Positioned(
-                top: 0,
-                left: 10,
-                child: FadeInDown(
-                    duration: Duration(milliseconds: 1600),
-                    child: Container(
-                      margin: EdgeInsets.only(top: 100),
-                      child: Center(
-                        child: Text(
-                          "You must know your weakness \nbefore anyone.",
-                          style: Theme.of(context).textTheme.displayLarge,
-                        ),
-                      ),
-                    )),
-              ),
-              Positioned(
-                top: 120,
-                left: 10,
-                child: FadeInUp(
-                    duration: Duration(milliseconds: 1600),
-                    child: Container(
-                      margin: EdgeInsets.only(top: 100),
-                      child: Center(
-                        child: Text(
-                          "Everyone has a weakness, whether it's\nme or others.",
-                          style: Theme.of(context).textTheme.titleSmall,
-                        ),
-                      ),
-                    )),
-              ),
-            ]),
-          ),
-        ],
+      backgroundColor: AppColors.backgroundPageColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
       ),
     );
   }
