7cd717b11519bfc85868f96317f7ea9a3aac5336
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-10 23:22:26 +0200

    App - add QuestionState

---
+++ lib/bloc/bloc/question_state.dart
@@ -0,0 +1,12 @@
+part of 'question_bloc.dart';
+
+abstract class QuestionState extends Equatable {
+  const QuestionState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class QuestionInitial extends QuestionState {}
+
+class QuestionLoading extends QuestionState {}
