18e52a99fedd88c6d0683363c25021963f19dc2a
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-10 19:51:30 +0200

    App - update questionWidget add data and onResponse

--- lib/common_widget/questions/question_widget.dart
+++ lib/common_widget/questions/question_widget.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'custom_question_input.dart';
 
-final Map<String, Widget Function(dynamic data)> questionWidget = {
-  '1': (data) => CustomQuestionInput(data: data), //input
+final Map<String, Widget Function(dynamic data, Function(String) onResponse)>
+    questionWidget = {
+  '1': (data, onResponse) => CustomQuestionInput(
+        data: data,
+        onResponse: onResponse,
+      ),
+
+  // '2': (data, onResponse) => CustomQuestionCheckbox(data: data, onResponse: onResponse),
+  // '3': (data, onResponse) => CustomQuestionRadio(data: data, onResponse: onResponse),
 };
