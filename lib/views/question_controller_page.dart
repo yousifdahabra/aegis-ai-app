bd99e16e77b5a6f848eaf6e3f5c3f102456bfaf0
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-11 00:08:09 +0200

    App - update QuestionControllerPage add QuestionBloc

--- lib/views/question_controller_page.dart
+++ lib/views/question_controller_page.dart
@@ -1,8 +1,11 @@
+import 'package:ai_safety_app/utils/functions.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ai_safety_app/bloc/question/question_bloc.dart';
 import 'package:ai_safety_app/common_widget/custom_button.dart';
 import 'package:ai_safety_app/common_widget/questions/question_widget.dart';
-import 'package:flutter/material.dart';
 
-class QuestionControllerPage extends StatefulWidget {
+class QuestionControllerPage extends StatelessWidget {
   final String questionType;
   final Map<String, dynamic> questionData;
 
@@ -12,45 +15,31 @@ class QuestionControllerPage extends StatefulWidget {
     required this.questionData,
   });
 
-  @override
-  State<QuestionControllerPage> createState() => _QuestionControllerPageState();
-}
-
-class _QuestionControllerPageState extends State<QuestionControllerPage> {
-  String userResponse = '';
-
-  void handleNext() {
-    print('User response: $userResponse');
+  void handleNext(BuildContext context, String response) async {
+    final questionId = questionData['id'];
+    final userId = await Functions.getUserId();
   }
 
   @override
   Widget build(BuildContext context) {
-    final questionWidgetSelect = questionWidget[widget.questionType]?.call(
-      widget.questionData,
-      (response) {
-        setState(() {
-          userResponse = response;
-        });
-      },
-    );
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Test Question'),
         backgroundColor: Colors.blue,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            if (questionWidgetSelect != null)
-              Expanded(child: questionWidgetSelect),
-            const SizedBox(height: 20),
-            CustomButton(
-              text: 'Next',
-              onPressed: handleNext,
-            ),
-          ],
+      body: BlocListener<QuestionBloc, QuestionState>(
+        listener: (context, state) {},
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              CustomButton(
+                text: 'Next',
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
