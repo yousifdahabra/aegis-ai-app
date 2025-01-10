779da3005494ed7c341e24d3243346d4dd202b24
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-10 20:16:35 +0200

    App - update questionWidget add CustomCheckbox

--- lib/common_widget/questions/question_widget.dart
+++ lib/common_widget/questions/question_widget.dart
@@ -1,3 +1,4 @@
+import 'package:ai_safety_app/common_widget/questions/custom_checkbox.dart';
 import 'package:ai_safety_app/common_widget/questions/custom_radio_button.dart';
 import 'package:flutter/material.dart';
 import 'custom_question_input.dart';
@@ -10,6 +11,5 @@ final Map<String, Widget Function(dynamic data, Function(String) onResponse)>
       ),
   '2': (data, onResponse) =>
       CustomRadioButton(data: data, onResponse: onResponse),
-
-  // '3': (data, onResponse) => CustomQuestionRadio(data: data, onResponse: onResponse),
+  '3': (data, onResponse) => CustomCheckbox(data: data, onResponse: onResponse),
 };
