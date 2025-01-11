5dfa9e02d82f46983a04b44753b08e8811ef8c6a
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-11 13:05:42 +0200

    App - add CustomVoiceInteractions

---
+++ lib/common_widget/questions/custom_voice_interactions.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomVoiceInteractions extends StatefulWidget {
+  const CustomVoiceInteractions({super.key});
+
+  @override
+  State<CustomVoiceInteractions> createState() =>
+      _CustomVoiceInteractionsState();
+}
+
+class _CustomVoiceInteractionsState extends State<CustomVoiceInteractions> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
