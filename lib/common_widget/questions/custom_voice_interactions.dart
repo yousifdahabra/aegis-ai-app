43a2f33c384e30dab06ef388a4d5a47d97b9ddcd
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-11 20:31:32 +0200

    App - update CustomVoiceInteractions add startListening

--- lib/common_widget/questions/custom_voice_interactions.dart
+++ lib/common_widget/questions/custom_voice_interactions.dart
@@ -4,10 +4,12 @@ import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class CustomVoiceInteractions extends StatefulWidget {
   final String questionText;
+  final Function(String) onResponse;
 
   const CustomVoiceInteractions({
     super.key,
     required this.questionText,
+    required this.onResponse,
   });
 
   @override
@@ -33,6 +35,33 @@ class _CustomVoiceInteractionsState extends State<CustomVoiceInteractions> {
     await _flutterTts.setSpeechRate(0.5);
 
     await _flutterTts.speak(text);
+  }
+
+  Future<void> _startListening() async {
+    bool available = await _speechToText.initialize(
+      onStatus: (status) => print("STT Status: $status"),
+      onError: (error) => print("STT Error: $error"),
+    );
+
+    if (available) {
+      setState(() {
+        _isListening = true;
+        _userResponse = "";
+      });
+
+      _speechToText.listen(
+        onResult: (result) {
+          setState(() {
+            _userResponse = result.recognizedWords;
+          });
+        },
+      );
+    } else {
+      setState(() {
+        _isListening = false;
+      });
+      print("Speech recognition not available");
+    }
   }
 
   @override
