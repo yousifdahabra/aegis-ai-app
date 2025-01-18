21c9281f2bb44eb033630aba4922451522ceed83
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-18 23:18:19 +0200

    App - update NavigationMenu

--- lib/common_widget/navigation_menu.dart
+++ lib/common_widget/navigation_menu.dart
@@ -1,9 +1,9 @@
-import 'package:ai_safety_app/views/auth/login.dart';
+import 'package:ai_safety_app/views/auth/profile.dart';
 import 'package:ai_safety_app/views/list/add_expert_request.dart';
+import 'package:ai_safety_app/views/list/expert_request.dart';
 import 'package:ai_safety_app/views/list/test_list.dart';
 import 'package:ai_safety_app/views/test_page.dart';
 import 'package:flutter/material.dart';
-import 'package:ai_safety_app/data/repositories/auth_repository.dart';
 
 class NavigationMenu extends StatefulWidget {
   const NavigationMenu({super.key});
@@ -17,42 +17,25 @@ class _NavigationMenuState extends State<NavigationMenu> {
 
   final List<Widget> _pages = [
     Center(child: TestList()),
-    Center(child: AddExpertRequest()),
+    Center(child: ExpertRequest()),
     Center(child: TestPage()),
-    const SizedBox(),
+    Center(child: Profile()),
   ];
-
-  Future<void> _logout() async {
-    final authRepository = AuthRepository();
-    await authRepository.logout();
-
-    if (!mounted) return;
-
-    Navigator.pushAndRemoveUntil(
-      context,
-      MaterialPageRoute(builder: (context) => const Login()),
-      (route) => false,
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _selectedIndex == 3 ? const SizedBox() : _pages[_selectedIndex],
+      body: _pages[_selectedIndex],
       bottomNavigationBar: NavigationBar(
         backgroundColor: Colors.white,
-        indicatorColor: const Color.fromARGB(122, 40, 156, 210),
+        indicatorColor: Color.fromARGB(122, 40, 156, 210),
         selectedIndex: _selectedIndex,
-        onDestinationSelected: (int index) async {
-          if (index == 3) {
-            await _logout();
-          } else {
-            setState(() {
-              _selectedIndex = index;
-            });
-          }
+        onDestinationSelected: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
         },
-        destinations: const [
+        destinations: [
           NavigationDestination(
             icon: Icon(Icons.list),
             label: 'Test List',
@@ -66,8 +49,8 @@ class _NavigationMenuState extends State<NavigationMenu> {
             label: 'New Test',
           ),
           NavigationDestination(
-            icon: Icon(Icons.logout),
-            label: 'Logout',
+            icon: Icon(Icons.person),
+            label: 'Profile',
           ),
         ],
       ),
