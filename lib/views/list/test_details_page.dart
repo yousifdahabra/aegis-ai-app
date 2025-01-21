8ee0395fa3823ec99fe1a491df54e76a9cfb26c6
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-21 20:13:57 +0200

    App - update TestDetailsPage add TestDetailsBloc

--- lib/views/list/test_details_page.dart
+++ lib/views/list/test_details_page.dart
@@ -12,43 +12,49 @@ class TestDetailsPage extends StatelessWidget {
     context.read<TestDetailsBloc>().add(FetchTestDetailsEvent(testId: testId));
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Test Details'),
-          backgroundColor: const Color(0xFF16354D),
-        ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Test Title',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              const SizedBox(height: 10),
-              Text(
-                'User: user_name',
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              const SizedBox(height: 5),
-              Text(
-                'Status:test_state',
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              const SizedBox(height: 5),
-              Text(
-                'Questions:questions_count ',
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              const SizedBox(height: 10),
-              const Divider(height: 20, thickness: 2),
-              Text(
-                'Questions',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              const SizedBox(height: 10),
-            ],
-          ),
-        ));
+      appBar: AppBar(
+        title: const Text('Test Details'),
+        backgroundColor: const Color(0xFF16354D),
+      ),
+      body: BlocBuilder<TestDetailsBloc, TestDetailsState>(
+        builder: (context, state) {
+          return SingleChildScrollView(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Test Title',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'User: user_name',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  'Status:test_state',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  'Questions:questions_count ',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                const SizedBox(height: 10),
+                const Divider(height: 20, thickness: 2),
+                Text(
+                  'Questions',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
+          );
+          return const SizedBox();
+        },
+      ),
+    );
   }
 }
