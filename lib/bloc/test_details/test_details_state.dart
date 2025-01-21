a261ccad59aef340f378da342bec2b06f8ddf82f
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-21 18:54:29 +0200

    App - update TestDetailsState add TestDetailsInitial,TestDetailsFailure and TestDetailsSuccess

--- lib/bloc/test_details/test_details_state.dart
+++ lib/bloc/test_details/test_details_state.dart
@@ -1,15 +1,31 @@
-abstract class TestDetailsState {}
+part of 'test_details_bloc.dart';
+
+sealed class TestDetailsState extends Equatable {
+  const TestDetailsState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class TestDetailsInitial extends TestDetailsState {}
 
 class TestDetailsLoading extends TestDetailsState {}
 
-class TestDetailsLoaded extends TestDetailsState {
-  final Map<String, dynamic> data;
+class TestDetailsSuccess extends TestDetailsState {
+  final Map<String, dynamic> test;
+  final List<Map<String, dynamic>> questions;
 
-  TestDetailsLoaded(this.data);
+  const TestDetailsSuccess({required this.test, required this.questions});
+
+  @override
+  List<Object?> get props => [test, questions];
 }
 
-class TestDetailsError extends TestDetailsState {
-  final String message;
+class TestDetailsFailure extends TestDetailsState {
+  final String error;
 
-  TestDetailsError(this.message);
+  const TestDetailsFailure(this.error);
+
+  @override
+  List<Object?> get props => [error];
 }
