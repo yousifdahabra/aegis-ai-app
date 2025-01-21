403f5e6261de8466fbfdcedbddd3ba30e30aeed6
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-21 18:55:29 +0200

    App - update TestDetailsBloc add TestDetailsFailure

--- lib/bloc/test_details/test_details_bloc.dart
+++ lib/bloc/test_details/test_details_bloc.dart
@@ -1,20 +1,31 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'test_details_event.dart';
-import 'test_details_state.dart';
 import 'package:ai_safety_app/data/repositories/tests_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'test_details_event.dart';
+part 'test_details_state.dart';
 
 class TestDetailsBloc extends Bloc<TestDetailsEvent, TestDetailsState> {
-  final TestsRepository testsRepository = TestsRepository();
+  final TestsRepository testsRepository;
 
-  TestDetailsBloc(TestsRepository testsRepository)
-      : super(TestDetailsLoading()) {
+  TestDetailsBloc(this.testsRepository) : super(TestDetailsInitial()) {
     on<FetchTestDetailsEvent>((event, emit) async {
       emit(TestDetailsLoading());
+
       try {
-        final data = await testsRepository.getTestDetails(event.testId);
-        emit(TestDetailsLoaded(data));
+        final response = await testsRepository.getTestDetails(event.testId);
+        if (response['status']) {
+          emit(TestDetailsSuccess(
+            test: response['data']['test'],
+            questions: response['data']['question'],
+          ));
+        } else {
+          emit(
+              TestDetailsFailure(response['message'] ?? 'Error loading test.'));
+        }
       } catch (e) {
-        emit(TestDetailsError(e.toString()));
+        emit(TestDetailsFailure(e.toString()));
       }
     });
   }
