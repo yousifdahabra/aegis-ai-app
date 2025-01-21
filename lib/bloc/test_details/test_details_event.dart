d7ea00b1e956822eb7d78243183372420897b8ba
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-21 18:54:57 +0200

    App - update TestDetailsEvent add immutable

--- lib/bloc/test_details/test_details_event.dart
+++ lib/bloc/test_details/test_details_event.dart
@@ -1,7 +1,10 @@
+part of 'test_details_bloc.dart';
+
+@immutable
 abstract class TestDetailsEvent {}
 
 class FetchTestDetailsEvent extends TestDetailsEvent {
   final int testId;
 
-  FetchTestDetailsEvent(this.testId);
+  FetchTestDetailsEvent({required this.testId});
 }
